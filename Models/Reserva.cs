bfd0ef38a4c8028bcf7f01cbe254c76e1fd174cf
Tayná Vicente Silva <taynavicente2019@gmail.com>
2024-09-26 15:03:36 -0300

    Desafio Concluído

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,16 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            foreach (var hospede in hospedes)
             {
-                Hospedes = hospedes;
-            }
-            else
-            {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                if (hospedes.Count <= Suite.Capacidade)
+                {
+                    Hospedes = hospedes;
+                }
+                else
+                {
+                    throw new Exception("O número de hóspedes recebido deve ser menor ou igual à capacidade!");
+                }
             }
         }
 
@@ -35,26 +35,19 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            var valor_diaria = DiasReservados * Suite.ValorDiaria;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor_diaria *= 0.90m;
             }
 
-            return valor;
+            return valor_diaria;
         }
     }
 }
